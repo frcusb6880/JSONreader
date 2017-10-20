1baefaa13aeb825198c092529c293823213b1eda
rsburugula <rsburugula@ramanjaneyasmbp.watson.ibm.com>
2017-10-20 00:27:57 -0400

    test

---
+++ src/BasicJSONreader/basicJSONreader.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package BasicJSONreader;
+
+import com.google.gson.*;
+
+
+/**
+ * @author burugula
+ *
+ */
+public class basicJSONreader {
+
+    /**
+     * 
+     */
+    public basicJSONreader() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
